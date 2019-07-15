9182715628da54804efe10ec7675c0d1a1b82ffa
Shane Calder <shanescottcalder@gmail.com>
2019-07-15 08:05:46 -0600

    Create main.dart
    Used flutter to build app in android studio

---
+++ pick_a_pic_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+
+// define a main function
+void main() {
+  // create a widget to show text
+
+
+  // get widget to show text on screen
+  runApp(App());
+}
